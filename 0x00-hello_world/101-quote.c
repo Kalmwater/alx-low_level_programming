206768c47679ffda883f1bfd6298f3d5b343ae68
kalmwater <captain obinna nwandu>
2022-07-01 11:17:28 -0700

    my commit o c tas

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,8 @@
 #include <unistd.h>
 
 /**
-* main - prints exactly "and that piece of art is useful"
-* - Dora korpar, 2015-10-19",
-* followed by anew line, to the standard error.
-* Return: Always 0 (Success)
+* main - Entry point
+* Return: Always 1 (Error)
 */
 int main(void)
 {
