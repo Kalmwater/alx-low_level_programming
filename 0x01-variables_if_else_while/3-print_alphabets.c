9b98a3f08440839e81d58a50e0c02f3aef47e403
kalmwater <captain obinna nwandu>
2022-07-02 20:42:24 -0700

    commit now

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,16 @@
 */
 int main(void)
 {
-	int ch;
-	for (ch = 97; ch <= 122; ch++)
-	{
-		putchar(ch);
-	}
-	for (ch = 65; ch <= 90; ch++)
-	{
-		putchar(ch);
-	}
-	putchar(10); /* this is an ascii code for new line*/
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10); /* this is an ascii code for new line*/
 
-	return (0);
+return (0);
 }
