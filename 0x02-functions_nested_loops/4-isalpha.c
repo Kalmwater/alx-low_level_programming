2057407a1035e194b35c681b51d2e3383b77334c
kalmwater <captain obinna nwandu>
2022-07-05 13:49:34 -0700

    nested while loop

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
 /**
-* _isalpha - checks for alphabetical letters
-* @c: a charter to be checked on 
-* Return: returns 0 or 1 depending on condition
+* _isalpha - function to check if c is a letter, lowercase or uppercase
+* @c: is the int that will use for the argument of the function
+* Return: 0
 */
 int _isalpha(int c)
 {
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+return (0);
 }
