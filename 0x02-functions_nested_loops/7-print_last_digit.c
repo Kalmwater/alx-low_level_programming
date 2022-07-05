e700fb9a8bdc6b67ad3bf0721d45c9b48c0e1f65
kalmwater <captain obinna nwandu>
2022-07-05 14:38:35 -0700

    nested while loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* _abs - function that computes the absolute value of an integer
-* @c:  is the int that will use for the argument of the function
-* Return: 0
+* print_last_digit - prints the last digit of a number
+* @n: the int to extract the last digit from
+* Return: value of the last digit
 */
-int _abs(int c)
+int print_last_digit(int n)
 {
-if (c > 0 || c == 0)
-{
-return (c);
-}
-else
-return (c * -1);
+int a;
+if (n < 0)
+n = -n;
+a = n % 10;
+if (a < 0)
+a = -a;
+_putchar(a + '0');
+return (a);
 }
