05894a6cc0c338373f72ceb67039383c60d3a661
kalmwater <captain obinna nwandu>
2022-07-08 12:01:21 -0700

    more loop

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,12 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int isPrime(long int n);
+long int biggestFactor(long int a);
+void print_number(int n);
+int numDigits(int n);
 
 #endif
