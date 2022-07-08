e12a793e44e7d8ad0d6e767a3d7c90927d033fcf
kalmwater <captain obinna nwandu>
2022-07-08 11:20:49 -0700

    more loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
-* more_numbers -> printing more numbers
+* more_numbers - entry point
+* Description: Prints the numbers with _putchar
+* Return: void
 */
 void more_numbers(void)
 {
-	int x, y;
-
-	for (x = 0; x < 10; x++)
-	{
-		for (y = 0; y < 15; y++)
-		{
-			_putchar(y + '0');
-		}
-		_putchar('\n');
-	}
+int number, tens, units, row;
+for (row = 1; row <= 10; row++)
+{
+for (number = 0; number <= 14; number++)
+{
+tens = number / 10;
+units = number % 10;
+if (number > 9)
+_putchar(tens + '0');
+_putchar(units + '0');
+}
+_putchar('\n');
+}
 }
